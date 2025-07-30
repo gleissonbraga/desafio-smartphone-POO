331221b783e58cfbe3ddfa50a3888903544cf8da
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2025-07-30 20:42:42 -0300

    desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia celular = new Nokia("51999303193", "Nokia 2", "1564648590845085", 128);
+Iphone celularDois = new Iphone("51999999999", "Iphone 16", "156484897029125", 256);
+
+
+
+celular.InstalarAplicativo("Instagram");
+celularDois.InstalarAplicativo("Youtube");
